322ed9833066da5e0e1cac3893db458fbd530ca8
Kolia Kapiernik <kapiernik27@gmail.com>
2021-04-18 01:36:11 +0300

    init

---
+++ tabs.js
@@ -0,0 +1,40 @@
+function tabs(tabsSelector, contentSelector, parentSelector, activeClass) {
+    let tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(contentSelector),
+        tabsParent = document.querySelector(parentSelector);
+
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', function (event) {
+        const target = event.target;
+        if (target && target.classList.contains(tabsContent.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+export default tabs;
